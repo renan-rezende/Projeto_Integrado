76b798fbff385eac5da478db4c4ce7333f69b275
Pedro H. S. Santos <127870288+Pedro-hssouza@users.noreply.github.com>
2023-06-23 14:34:11 -0300

    Update script.sql

--- Banco_de_dados/script.sql
+++ Banco_de_dados/script.sql
@@ -3,9 +3,9 @@ CREATE TABLE cargo (
                 area_de_atuacao         VARCHAR(100) NOT NULL,
                 CONSTRAINT pk_cargo   PRIMARY KEY (cargo_id)-- Criação da pk da tabela
 );
-COMMENT ON TABLE cargo IS 'Tabela de cargos da empresa'
-COMMENT ON COLUMN cargo.cargo_id IS 'Pk da tabela que identifica cada cargo de maneira unica'
-COMMENT ON COLUMN cargo.area_de_atuacao IS 'area de atuação de cada cargo'
+COMMENT ON TABLE cargo IS 'Tabela de cargos da empresa';
+COMMENT ON COLUMN cargo.cargo_id IS 'Pk da tabela que identifica cada cargo de maneira unica';
+COMMENT ON COLUMN cargo.area_de_atuacao IS 'area de atuação de cada cargo';
 
 CREATE TABLE projetos (
                 projeto_id              NUMERIC(10) NOT NULL,
@@ -14,11 +14,11 @@ CREATE TABLE projetos (
                 descricao_projeto       VARCHAR(3000) NOT NULL,
                 CONSTRAINT pk_projetos  PRIMARY KEY (projeto_id)-- Criação da pk da tabela
 );
-COMMENT ON TABLE projetos IS 'Tabela de projetos da fortes'
-COMMENT ON COLUMN projetos.projeto_id IS 'Pk da tabela que identifica cada projeto de maneira unica'
-COMMENT ON COLUMN projetos.nome IS 'Nome do projeto'
-COMMENT ON COLUMN projetos.img_projeto IS 'Imagem do projeto'
-COMMENT ON COLUMN projetos.descricao_projeto IS 'Descrição do projeto'
+COMMENT ON TABLE projetos IS 'Tabela de projetos da fortes';
+COMMENT ON COLUMN projetos.projeto_id IS 'Pk da tabela que identifica cada projeto de maneira unica';
+COMMENT ON COLUMN projetos.nome IS 'Nome do projeto';
+COMMENT ON COLUMN projetos.img_projeto IS 'Imagem do projeto';
+COMMENT ON COLUMN projetos.descricao_projeto IS 'Descrição do projeto';
 
 
 CREATE TABLE comunidade (
@@ -28,26 +28,28 @@ CREATE TABLE comunidade (
                 descricao_comuni        VARCHAR(3000) NOT NULL,
                 CONSTRAINT pk_comunidae    PRIMARY KEY (comuni_id)-- Criação da pk da tabela
 );
-COMMENT ON TABLE comunidade IS 'Tabela da comunidades'
-COMMENT ON COLUMN comunidade.comuni_id IS 'PK da tabela que identifica cada comunidade de maneira única'
-COMMENT ON COLUMN comunidade.nome IS 'Nome de cada comunidade'
-COMMENT ON COLUMN comunidade.foto IS 'foto da comunidade'
-COMMENT ON COLUMN comunidade.descricao_comuni IS 'descrição da comunidade'
+COMMENT ON TABLE comunidade IS 'Tabela da comunidades';
+COMMENT ON COLUMN comunidade.comuni_id IS 'PK da tabela que identifica cada comunidade de maneira única';
+COMMENT ON COLUMN comunidade.nome IS 'Nome de cada comunidade';
+COMMENT ON COLUMN comunidade.foto IS 'foto da comunidade';
+COMMENT ON COLUMN comunidade.descricao_comuni IS 'descrição da comunidade';
 
 CREATE TABLE colaborador (
                 colab_id                NUMERIC(10) NOT NULL,
                 nome                    VARCHAR(100) NOT NULL,
                 data_de_nascimento      DATE NOT NULL,
+                email                   VARCHAR(100) NOT NULL,
                 cargo_id                NUMERIC(10) NOT NULL,
                 sobre_mim               VARCHAR(3000),
                 CONSTRAINT pk_colaborador   PRIMARY KEY (colab_id)-- Criação da pk da tabela
 );
 COMMENT ON TABLE colaborador IS 'Tabela com os dados do perfil de cada colaborador da empresa.';
-COMMENT ON COLUMN colaborador.colab_id IS 'Pk da tabela que identifica cada colaborador unicamente'
-COMMENT ON COLUMN colaborador.nome IS 'Nome do colaborador '
-COMMENT ON COLUMN colaborador.data_de_nascimento IS 'data de nascimento do colaborador'
-COMMENT ON COLUMN colaborador.cargo_id IS 'Fk da tabela que vem da tabela cargos'
-COMMENT ON COLUMN colaborador.sobre_mim IS 'Uma descrição que o proprio colaborador informa'
+COMMENT ON COLUMN colaborador.colab_id IS 'Pk da tabela que identifica cada colaborador unicamente';
+COMMENT ON COLUMN colaborador.nome IS 'Nome do colaborador ';
+COMMENT ON COLUMN colaborador.data_de_nascimento IS 'data de nascimento do colaborador';
+COMMENT ON COLUMN colaborador.email IS 'Email corporativo do colaborador esse atributo é único ';
+COMMENT ON COLUMN colaborador.cargo_id IS 'Fk da tabela que vem da tabela cargos';
+COMMENT ON COLUMN colaborador.sobre_mim IS 'Uma descrição que o proprio colaborador informa';
 
 
 CREATE TABLE colaborador_comunidade (
@@ -55,9 +57,9 @@ CREATE TABLE colaborador_comunidade (
                 comuni_id NUMERIC(10)   NOT NULL,
                 CONSTRAINT pk_colaborador_comunidade      PRIMARY KEY (colab_id, comuni_id)-- Criação da pk da tabela
 );
-COMMENT ON TABLE colaborador_comunidade IS 'Tebela que contém duas pfk uma de comunidade e outra de colaborador'
-COMMENT ON COLUMN colaborador_comunidade.colab_id IS 'Pfk da tabela pois ela se torna uma pk na tabela mesmo vindo de outra tabela'
-COMMENT ON COLUMN colaborador_comunidade.colab_id IS 'Pfk da tabela , ela vem da tabela colaborador'
+COMMENT ON TABLE colaborador_comunidade IS 'Tebela que contém duas pfk uma de comunidade e outra de colaborador';
+COMMENT ON COLUMN colaborador_comunidade.colab_id IS 'Pfk da tabela pois ela se torna uma pk na tabela mesmo vindo de outra tabela';
+COMMENT ON COLUMN colaborador_comunidade.colab_id IS 'Pfk da tabela , ela vem da tabela colaborador';
 
 
 CREATE TABLE colaborador_projeto (
@@ -65,9 +67,9 @@ CREATE TABLE colaborador_projeto (
                 projeto_id NUMERIC(10)  NOT NULL,
                 CONSTRAINT pk_colaborador_projeto      PRIMARY KEY (colab_id, projeto_id)-- Criação da pk da tabela
 );
-COMMENT ON TABLE colaborador_projeto. IS 'Tabela que contem pk da tabela colaborador e de projetos'
-COMMENT ON COLUMN colaborador_projeto.colab_id IS 'Pfk da tabela pois ela se torna uma pk na tabela mesmo vindo de outra tabela'
-COMMENT ON COLUMN colaborador_projeto.projeto_id IS 'Pfk da tabela pois ela se torna uma pk na tabela mesmo vindo de outra tabela'
+COMMENT ON TABLE colaborador_projeto. IS 'Tabela que contem pk da tabela colaborador e de projetos';
+COMMENT ON COLUMN colaborador_projeto.colab_id IS 'Pfk da tabela pois ela se torna uma pk na tabela mesmo vindo de outra tabela';
+COMMENT ON COLUMN colaborador_projeto.projeto_id IS 'Pfk da tabela pois ela se torna uma pk na tabela mesmo vindo de outra tabela';
 
 
 CREATE TABLE skills (
@@ -76,10 +78,10 @@ CREATE TABLE skills (
                 soft_skill VARCHAR(100)     NOT NULL,
                 CONSTRAINT pk_skills          PRIMARY KEY (colab_id)-- Criação da pk da tabela
 );
-COMMENT ON TABLE skills IS 'Tabela de skills dos colaboradores'
-COMMENT ON COLUMN skills.colab_id IS 'Pfk da tabela , quem vem da tabela colaborador'
-COMMENT ON COLUMN skills.hard_skill IS 'descrição das hards skills do colaborador'
-COMMENT ON COLUMN skills.soft_skill IS 'descrição das soft skills do colaborador'
+COMMENT ON TABLE skills IS 'Tabela de skills dos colaboradores';
+COMMENT ON COLUMN skills.colab_id IS 'Pfk da tabela , quem vem da tabela colaborador';
+COMMENT ON COLUMN skills.hard_skill IS 'descrição das hards skills do colaborador';
+COMMENT ON COLUMN skills.soft_skill IS 'descrição das soft skills do colaborador';
 
 
 ALTER TABLE colaborador ADD CONSTRAINT cargo_colaborador_fk
